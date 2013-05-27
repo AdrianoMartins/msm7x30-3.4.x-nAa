0e0b8d9c6b56903cd3484eb0bdcc81e9a23e143f
Adriano Martins <adrianomartins@gmail.com>
2013-05-26 22:40:59 -0300

    Overclock and disable lower frequency that use the same  voltage of high frequency

--- arch/arm/mach-msm/acpuclock-7x30.c
+++ arch/arm/mach-msm/acpuclock-7x30.c
@@ -91,13 +91,16 @@ static struct clock_state drv_state = { 0 };
 static struct clkctl_acpu_speed *backup_s;
 
 static struct pll pll2_tbl[] = {
-	{ 42, 0, 1, 0 }, /* 42 * 19,2MHz =  806,4 MHz */
-	{ 47, 1, 3, 0 }, /* 47 * 19,2MHz =  902,4 MHz */
-	{ 53, 1, 3, 0 }, /* 53 * 19,2MHz = 1017,6 MHz */
-	{ 58, 1, 3, 0 }, /* 58 * 19,2MHz = 1113,6 MHz */
-	{ 63, 1, 3, 0 }, /* 63 * 19,2MHz = 1209,6 MHz */
-	{ 68, 1, 3, 0 }, /* 68 * 19,2MHz = 1305,6 MHz */
-	{ 73, 0, 1, 0 }, /* 73 * 19,2MHz = 1401,6 MHz */
+	{42, 0, 1, 0},		/* 806 MHz */
+	{48, 1, 3, 0},		/* 921 MHz */
+	{53, 1, 3, 0},		/* 1024 MHz */
+	{58, 1, 3, 0},		/* 1113 MHz */
+	{63, 1, 3, 0},		/* 1200 MHz */
+	{68, 1, 3, 0},		/* 1305 MHz */
+	{73, 1, 3, 0},		/* 1401 MHz */
+	{78, 1, 3, 0},		/* 1516 MHz */
+	{83, 1, 3, 0},		/* 1612 MHz */
+	{88, 1, 3, 0},		/* 1708 MHz */
 };
 
 /* Use negative numbers for sources that can't be enabled/disabled */
@@ -121,27 +124,27 @@ static struct clk *acpuclk_sources[MAX_SOURCE];
  * know all the h/w requirements.
  */
 static struct clkctl_acpu_speed acpu_freq_tbl[] = {
-	{ 0, 24576,  LPXO, 0, 0,  30720000,  900, VDD_RAW(900) },
-	{ 0, 61440,  PLL_3,    5, 11, 61440000,  900, VDD_RAW(900) },
-	{ 1, 122880, PLL_3,    5, 5,  61440000,  900, VDD_RAW(900) },
-	{ 0, 184320, PLL_3,    5, 4,  61440000,  900, VDD_RAW(900) },
-	{ 0, MAX_AXI_KHZ, AXI, 1, 0, 61440000, 900, VDD_RAW(900) },
-	{ 1, 245760, PLL_3,    5, 2,  61440000,  900, VDD_RAW(900) },
-	{ 1, 368640, PLL_3,    5, 1,  122800000, 900, VDD_RAW(900) },
-	/* AXI has MSMC1 implications. See above. */
-	{ 1, 460800, PLL_1,    2, 0,  153600000, 950, VDD_RAW(950) },
-	{ 1, 768000, PLL_1,    2, 0,  153600000, 1050, VDD_RAW(1050) },
-	/*
-	 * AXI has MSMC1 implications. See above.
-	 */
-	{ 1, 806400,  PLL_2, 3, 0, UINT_MAX, 1100, VDD_RAW(1100), &pll2_tbl[0]},
-	{ 1, 902400,  PLL_2, 3, 0, UINT_MAX, 1125, VDD_RAW(1125), &pll2_tbl[1]},
-	{ 1, 1017600, PLL_2, 3, 0, UINT_MAX, 1175, VDD_RAW(1175), &pll2_tbl[2]},
-	{ 1, 1113600, PLL_2, 3, 0, UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[3]},
-	{ 1, 1209600, PLL_2, 3, 0, UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[4]},
-	{ 1, 1305600, PLL_2, 3, 0, UINT_MAX, 1225, VDD_RAW(1225), &pll2_tbl[5]},
-	{ 1, 1401600, PLL_2, 3, 0, UINT_MAX, 1250, VDD_RAW(1250), &pll2_tbl[6]},
-	{ 0 }
+	{0,   24576,     LPXO, 0,  0,  30720000,  900, VDD_RAW(900)},
+	{0,   61440,    PLL_3, 5, 11,  61440000,  900, VDD_RAW(900)},
+	{0,  122880,    PLL_3, 5,  5,  61440000,  900, VDD_RAW(900)},
+	{0,  184320,    PLL_3, 5,  4,  61440000,  900, VDD_RAW(900)},
+	{0,  245760,    PLL_3, 5,  2,  61440000,  900, VDD_RAW(900)},
+	{1,  368640,    PLL_3, 5,  1, 122800000,  900, VDD_RAW(900)},
+	{1,  460800,    PLL_1, 2,  0, 153600000,  950, VDD_RAW(950)},
+	{1,  576000,    PLL_1, 2,  0, 153600000, 1000, VDD_RAW(1000)},
+	{0,  652800,    PLL_1, 2,  0, 153600000, 1050, VDD_RAW(1050)},
+	{1,  768000,    PLL_1, 2,  0, 153600000, 1050, VDD_RAW(1050)},
+	{1,  806400,    PLL_2, 3,  0,  UINT_MAX, 1100, VDD_RAW(1100), &pll2_tbl[0]},
+	{1,  921600,    PLL_2, 3,  0,  UINT_MAX, 1150, VDD_RAW(1150), &pll2_tbl[1]},
+	{0, 1024000,    PLL_2, 3,  0,  UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[2]},
+	{0, 1113000,    PLL_2, 3,  0,  UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[3]},
+	{1, 1200000,    PLL_2, 3,  0,  UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[4]},
+	{0, 1305600,    PLL_2, 3,  0,  UINT_MAX, 1250, VDD_RAW(1250), &pll2_tbl[5]},
+	{1, 1401600,    PLL_2, 3,  0,  UINT_MAX, 1250, VDD_RAW(1250), &pll2_tbl[6]},
+	{1, 1516800,    PLL_2, 3,  0,  UINT_MAX, 1300, VDD_RAW(1300), &pll2_tbl[7]},
+	{1, 1612800,    PLL_2, 3,  0,  UINT_MAX, 1350, VDD_RAW(1350), &pll2_tbl[8]},
+	{1, 1708800,    PLL_2, 3,  0,  UINT_MAX, 1400, VDD_RAW(1400), &pll2_tbl[9]},
+	{0}
 };
 
 static int acpuclk_set_acpu_vdd(struct clkctl_acpu_speed *s)
